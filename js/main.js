cb1bae568def83e47d057d2471e6591348224367
Shanna Ginos <Shanna@Dustins-MacBook-Pro.local>
2021-03-24 10:36:11 -0600

    added select course functionality, started on teebox select functionality

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
-//get courses
-function getAvailableCourses() {
-    return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
-      function (response) {
-        return response.json();
-      }
-    );
+const getCoursesPromise = fetch("https://golf-courses-api.herokuapp.com/courses", {
+  method: "GET",
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+const courseDataPromise = getCoursesPromise.then((response) => {
+  console.log(response);
+  return response.json();
+});
+
+courseDataPromise.then(data => {
+  let courseOptionsHtml = '';
+  data.courses.forEach(course => {
+    courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+  });
+  document.getElementById('course-select').innerHTML = courseOptionsHtml;
+});
+
+courseDataPromise.then(data => {
+  let teeBoxSelectHtml = ''
+  data.teeBoxes.forEach(function (teeBox, index) {
+    teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${teeBox.totalYards} yards</option>`
+  });
+  document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
+})/
+
+function print() {
+  //when course and teebox are selected, print all the info for that course
+  //print par, yardage, and handicap info
+  //print names entered in and print scores when entered
 }
-
-console.log(getAvailableCourses());
-
-//display course options in html
-let courseOptionsHtml = '';
-courses.forEach((course) => {
- courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
-});
-document.getElementById('course-select').innerHTML = courseOptionsHtml;
- 
-//tee box select
-let teeBoxSelectHtml = ''
-teeBoxes.forEach(function (teeBox, index) {
-  teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${teeBox.totalYards} yards</option>`
-});
-document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
-
