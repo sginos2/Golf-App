d63e24e544ec968b343bc7b14e39ba527daf83b7
Shanna Ginos <Shanna@Dustins-MacBook-Pro.local>
2021-04-06 09:12:02 -0600

    completed addNewPlayer function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let courseData;
-let players = {};
+let players = [];
 
 document.getElementById('course-select').addEventListener('change', function(event) {
   let courseId = event.target.value;
@@ -11,6 +11,40 @@ document.getElementById('tee-box-select').addEventListener('change', function(ev
   let teeBox = event.target.value;
   getTeeBoxInfo(teeBox);
 });
+
+document.getElementById('player1').addEventListener('keydown', event => {
+  if (event.key == 'Enter') {
+    addNewPlayer('player1');
+  }
+})
+
+document.getElementById('player2').addEventListener('keydown', event => {
+  if (event.key == 'Enter') {
+    addNewPlayer('player2');
+  }
+})
+
+document.getElementById('player3').addEventListener('keydown', event => {
+  if (event.key == 'Enter') {
+    addNewPlayer('player3');
+  }
+})
+
+document.getElementById('player4').addEventListener('keydown', event => {
+  if (event.key == 'Enter') {
+    addNewPlayer('player4');
+  }
+})
+
+/*printScores(){
+  let sum = 0;
+  for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  }
+  //put the value of sum in the 'total' column
+  //if players.player[playerId].scores.length =
+}
+*/
 
 const getCoursesPromise = fetch("https://golf-courses-api.herokuapp.com/courses", {
   method: "GET",
@@ -63,3 +97,23 @@ function getTeeBoxInfo(teeBox) {
     document.getElementById('handicap' + (index + 1)).innerText = hole.teeBoxes[teeBox].hcp;
   });
 }
+
+function addNewPlayer(playerId) {
+  const playerName = document.getElementById(playerId).value;
+  if (playerName) {
+    const newPlayer = new Player(playerName);
+    players.push(newPlayer);
+    document.getElementById(playerId).classList.add('form-control-plaintext');
+    document.getElementById(playerId).readOnly = true;
+  }
+}
+
+// function inputScore(playerId) {
+//   const playerScore = document.getElementById('p1score1').value;
+//   if (playerScore) {
+//     players.player[playerId].scores.push(playerScore);
+//     document.getElementById('p1score1').classList.add('form-control-plaintext');
+//     document.getElementById('p1scores1').readOnly = true;
+//     printScores();
+//   }
+// }
